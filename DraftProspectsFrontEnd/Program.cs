7c7692fa69fd318a56c1f0198056473c694fc275
SBrown510 <92278141+SBrown510@users.noreply.github.com>
2024-07-30 22:45:52 -0700

    Connected Webapp to API
    Connected Webapp to Backend API and tested using a call for Weather data from the backend.

--- DraftProspectsFrontEnd/Program.cs
+++ DraftProspectsFrontEnd/Program.cs
@@ -1,7 +1,28 @@
+using System.Net.Http.Headers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddHttpClient("AppClient", client => {
+    client.BaseAddress = new Uri("http://localhost:5277/");
+    client.DefaultRequestHeaders.Accept.Clear();
+    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+});
+
+builder.Services.AddTransient<ApiService>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
 
 var app = builder.Build();
 
@@ -18,7 +39,13 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseCors("AllowAll");
+
 app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.MapRazorPages();
 
